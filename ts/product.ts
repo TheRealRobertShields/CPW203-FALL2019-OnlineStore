a18296c19d495399667c38b27adc2cc0f39927d9
Robert Shields <roberthshields93@yahoo.com>
2019-11-12 13:50:52 -0800

    Created Product class in typescript

---
+++ ts/product.ts
@@ -0,0 +1,15 @@
+/**
+ * Represents a single shopping cart item.
+ */
+class Product{
+    name: string;
+    description: string;
+    price: Number;
+}
+// Test Code //
+/*
+let prod = new Product();
+prod.name = "blue thing";
+prod.description = "a blue thing";
+prod.price = 4.99;
+*/
